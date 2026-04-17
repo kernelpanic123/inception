c56d5703e6205abeb93609022672abbd8cc82782
kernelpanic <abdertouaitia@gmail.com>
2026-04-17 05:25:54 +0200

    l

--- srcs/requirements/mariadb/tools/help.sh
+++ srcs/requirements/mariadb/tools/help.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "Initialisation de MariaDB en mode sécurisé..."
+if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
 
-# 1. On lance MariaDB sans vérifier les permissions (indispensable si le socket bloque)
-# On le lance en arrière-plan
-mariadbd-safe --skip-grant-tables --skip-networking &
-
-# 2. On attend que le processus soit prêt
-until mariadb-admin ping >/dev/null 2>&1; do
-    echo "Attente du démarrage du daemon..."
-    sleep 1
-done
-
-echo "Configuration des accès pour : ${SQL_USER}"
-
-# 3. Puisqu'on est en --skip-grant-tables, on utilise 'mariadb' sans user/pass
-mariadb << EOF
+    cat << EOF > /tmp/init.sql
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
@@ -25,12 +12,8 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
-echo "Redémarrage de MariaDB en mode normal..."
+    mariadbd --user=mysql --bootstrap < /tmp/init.sql
+    rm -f /tmp/init.sql
+fi
 
-# 4. On arrête le mode safe proprement
-pkill mariadbd
-sleep 2
-
-# 5. On lance le vrai serveur (CMD)
-echo "MariaDB est prêt !"
 exec "$@"
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -13,7 +13,6 @@ if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
         --dbpass=$SQL_PASSWORD \
         --dbhost=mariadb:3306
 
-    # 3. Installe WordPress et crée l'utilisateur admin
     wp core install --allow-root \
         --url=$DOMAIN_NAME \
         --title=$SITE_TITLE \
@@ -21,7 +20,10 @@ if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
         --admin_password=$ADMIN_PASSWORD \
         --admin_email=$ADMIN_EMAIL
 
+    wp user create --allow-root \
+        $USER1_LOGIN $USER1_EMAIL \
+        --user_pass=$USER1_PASS \
+        --role=author
 fi
 
-# Lancer PHP-FPM au premier plan
 exec /usr/sbin/php-fpm8.2 -F
